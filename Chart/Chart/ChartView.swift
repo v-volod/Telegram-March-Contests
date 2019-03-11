fec362e78ff2b868712a7ea3f43c30cc2e6d8159
Vojko Vladimir <vojkovladimir@gmail.com>
2019-03-11 02:00:58 +0200

    Draw simple chart

--- Chart/Chart/ChartView.swift
+++ Chart/Chart/ChartView.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
+public protocol ChartViewDataSource: class {
+    func numberOfLines() -> Int
+    
+    func valuesFoxXAxis() -> [Int]
+    func valuesForYAxisForLine(_ line: Int) -> [Int]
+    
+    func colorForLine(_ line: Int) -> UIColor
+}
+
 @IBDesignable
 public class ChartView: UIView {
+    
+    open weak var dataSource: ChartViewDataSource?
 
     override public func draw(_ rect: CGRect) {
-        UIColor.black.setFill()
+        guard let dataSource = dataSource else { return }
         
-        let linePath = UIBezierPath(rect: rect)
-        linePath.move(to: CGPoint(x: rect.minX, y: rect.minY))
-        linePath.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
-        linePath.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
-        linePath.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-        linePath.stroke()
+        let numberOfLines = dataSource.numberOfLines()
+        let xCoordinates = dataSource.valuesFoxXAxis()
+        
+        guard numberOfLines > 0 && xCoordinates.count > 0 else { return }
+        
+        var maxValue: Int = dataSource.valuesForYAxisForLine(0).first!
+        
+        for line in 0..<numberOfLines {
+            maxValue = max(dataSource.valuesForYAxisForLine(line).max()!, maxValue)
+        }
+        
+        let xStep = rect.width / CGFloat(xCoordinates.count)
+        let yStep = rect.height / CGFloat(maxValue)
+        
+        
+        for line in 0..<numberOfLines {
+            let color = dataSource.colorForLine(line)
+            color.setStroke()
+            color.setFill()
+            
+            let linePath = UIBezierPath()
+            
+            let yCoordinates = dataSource.valuesForYAxisForLine(line)
+            
+            for index in 0..<xCoordinates.count {
+                let x = CGFloat(index) * xStep
+                let y = rect.height - yStep * CGFloat(yCoordinates[index])
+                let point = CGPoint(x: x, y: y)
+                
+                if index == 0 {
+                    linePath.move(to: point)
+                } else {
+                    linePath.addLine(to: point)
+                }
+            }
+            
+            linePath.stroke()
+        }
     }
 
 }
--- Statistic/View/ViewController.swift
+++ Statistic/View/ViewController.swift
@@ -7,18 +7,67 @@
 //
 
 import UIKit
+import Chart
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var chartView: ChartView!
+    
+    var chart: Chart? {
+        didSet {
+            chartView.setNeedsDisplay()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        chartView.dataSource = self
+        
         let repository = ChartRepository()
-        repository.load { _ in
+        repository.load { [weak self] result in
+            guard let controller = self else { return }
             
+            switch result {
+            case let .success(data):
+                if let chart = data.first {
+                    controller.chart = chart
+                }
+                break
+            case let .failure(error):
+                let alert = UIAlertController(title: error.localizedDescription, message: nil, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                controller.present(alert, animated: true, completion: nil)
+                break
+            }
         }
     }
 
 
 }
 
+// MARK: - ChartViewDataSource -
+
+extension ViewController: ChartViewDataSource {
+    func numberOfLines() -> Int {
+        return chart?.yCoordinates.count ?? 0
+    }
+    
+    func valuesFoxXAxis() -> [Int] {
+        return chart?.xCoordinates ?? []
+    }
+    
+    func valuesForYAxisForLine(_ line: Int) -> [Int] {
+        return chart?.yCoordinates[line] ?? []
+    }
+    
+    func colorForLine(_ line: Int) -> UIColor {
+        if let colorHex = chart?.colors[line], let color = UIColor(hex: colorHex) {
+            return color
+        }
+        
+        fatalError()
+    }
+    
+    
+}
