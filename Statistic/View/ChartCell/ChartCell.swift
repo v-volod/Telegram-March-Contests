728064e6e337b170fef4379e43dfaeac22dfd358
Vojko Vladimir <vojkovladimir@gmail.com>
2019-03-24 21:43:53 +0200

    Add chart empty state

--- Statistic/View/ChartCell/ChartCell.swift
+++ Statistic/View/ChartCell/ChartCell.swift
@@ -20,6 +20,8 @@ class ChartCell: UITableViewCell {
     @IBOutlet weak var chartView: ChartView!
     @IBOutlet weak var chartSlider: ChartRangeSlider!
     
+    @IBOutlet weak var noDataLabel: UILabel!
+    
     weak var delegate: ChartCellDelegate?
     
     override func awakeFromNib() {
@@ -39,11 +41,30 @@ class ChartCell: UITableViewCell {
         
         chartSlider.chart = chart
         chartSlider.range = chart.range
+        
+        updateVisibility(animated: true)
     }
     
     func update(animated: Bool = true) {
         chartView.update(animated: animated)
         chartSlider.update()
+        
+        updateVisibility(animated: animated)
+    }
+    
+    private func updateVisibility(animated: Bool = true) {
+        let isEmpty = chartSlider.chart.lines.filter({ $0.isEnabled }).isEmpty
+        
+        let alpha: CGFloat = isEmpty ? 0 : 1
+        
+        if (animated) {
+            UIView.animate(withDuration: 0.3) {
+                self.chartView.alpha = alpha
+                self.chartSlider.alpha = alpha
+                
+                self.noDataLabel.alpha = isEmpty ? 1.0 : 0.0
+            }
+        }
     }
 
     @IBAction func rangeDidChanged(_ sender: ChartRangeSlider, forEvent event: UIEvent) {
